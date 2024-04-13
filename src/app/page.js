b93f79df30d3c846b17eacf867c03025d6f4ff1b
justremember <nothingpersonal.qwerty@gmail.com>
2024-04-13 17:14:07 +0800

    Use noto sans jp as font. Bring back SSR.

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
+import Display from "@/app/components/Display";
 
+/*
 const DisplayNoSSR = dynamic(() => import("@/app/components/Display"), {
     ssr: false,
 });
+*/
 
 export default async function Home() {
     return (
         <div className="App">
-            <DisplayNoSSR />
+            <Display />
+            {/* <DisplayNoSSR /> */}
         </div>
     )
 }
